3f98e96c8231d0741d5bb48d868fdaab99a73b2d
Vaddiharika <152894901+Vaddiharika@users.noreply.github.com>
2025-07-15 14:59:33 +0530

    Time: 7 ms (57.13%), Space: 19.2 MB (97.66%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,6 +1,6 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         d=collections.Counter(nums)
-        for key,val in d.items():
-            if val>(len(nums)//2):
-                return key
+        for i,j in d.items():
+            if j>(len(nums)/2):
+                return i
